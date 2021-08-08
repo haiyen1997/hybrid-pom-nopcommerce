856b7e097aeaedc46c413e5c3646e4fdd70cbc4f
Vu Yen <haiyen025910@gmail.com>
2021-08-08 22:16:53 +0700

    Page Object

---
+++ Actions/pageObjects/MyAccountPageObject.java
@@ -0,0 +1,55 @@
+package pageObjects;
+
+import org.openqa.selenium.WebDriver;
+
+import commons.BasePage;
+import pageUIs.MyAccountPageUI;
+
+public class MyAccountPageObject extends BasePage{
+	private WebDriver driver;
+
+	public MyAccountPageObject(WebDriver _driver) {
+		this.driver = _driver;
+	}
+
+	public boolean isGenderMaleRadioSelected() {
+		waitForElementVisible(driver, MyAccountPageUI.GENDER_MALE_RADIO);
+		return isElementDisplayed(driver, MyAccountPageUI.GENDER_MALE_RADIO);
+	}
+
+	public String getFirstNameTextboxValue() {
+		waitForElementVisible(driver, MyAccountPageUI.FIRSTNAME_TEXTBOX);
+		return getAttribute(driver, MyAccountPageUI.FIRSTNAME_TEXTBOX, "value");
+	}
+
+	public String getLastNameTextboxValue() {
+		waitForElementVisible(driver, MyAccountPageUI.LASTNAME_TEXTBOX);
+		return getAttribute(driver, MyAccountPageUI.LASTNAME_TEXTBOX, "value");
+	}
+
+	public String getEmailTextboxValue() {
+		waitForElementVisible(driver, MyAccountPageUI.EMAIL_TEXTBOX);
+		return getAttribute(driver, MyAccountPageUI.EMAIL_TEXTBOX, "value");
+	}
+
+	public String getCompanyNameTextboxValue() {
+		waitForElementVisible(driver, MyAccountPageUI.COMPANY_TEXTBOX);
+		return getAttribute(driver, MyAccountPageUI.COMPANY_TEXTBOX, "value");
+	}
+
+	public String getPageDayDropdownValue() {
+		waitForElementVisible(driver, MyAccountPageUI.DAY_DROPDOWN);
+		return getFirstSelectedItemInDefaultDropdown(driver, MyAccountPageUI.DAY_DROPDOWN);
+	}
+
+	public String getPageMonthDropdownValue() {
+		waitForElementVisible(driver, MyAccountPageUI.MONTH_DROPDOWN);
+		return getFirstSelectedItemInDefaultDropdown(driver, MyAccountPageUI.MONTH_DROPDOWN);
+	}
+
+	public String getPageYearDropdownValue() {
+		waitForElementVisible(driver, MyAccountPageUI.YEAR_DROPDOWN);
+		return getFirstSelectedItemInDefaultDropdown(driver, MyAccountPageUI.YEAR_DROPDOWN);
+	}
+
+}
--- Actions/pageObjects/RegisterPageObject.java
+++ Actions/pageObjects/RegisterPageObject.java
@@ -1,5 +1,85 @@
 package pageObjects;
 
-public class RegisterPageObject {
+import org.openqa.selenium.WebDriver;
+
+import commons.BasePage;
+import pageUIs.RegisterPageUI;
+
+public class RegisterPageObject extends BasePage{
+	private WebDriver driver;
+
+	public RegisterPageObject(WebDriver _driver) {
+		this.driver = _driver;
+	}
+
+	public void clickToGenderMaleRadio() {
+		waitForElementClickable(driver, RegisterPageUI.GENDER_MALE_RADIO);
+		clickToElement(driver, RegisterPageUI.GENDER_MALE_RADIO);
+
+	}
+
+	public void enterToFirstNameTextbox(String firstName) {
+		waitForElementVisible(driver, RegisterPageUI.FIRSTNAME_TEXTBOX);
+		sendkeyToElement(driver, RegisterPageUI.FIRSTNAME_TEXTBOX, firstName);
+	}
+
+	public void enterToLastNameTextbox(String lastName) {
+		waitForElementVisible(driver, RegisterPageUI.LASTNAME_TEXTBOX);
+		sendkeyToElement(driver, RegisterPageUI.LASTNAME_TEXTBOX, lastName);
+	}
+
+	public void selectDayDropDown(String day) {
+		waitForElementVisible(driver, RegisterPageUI.DAY_DROPDOWN);
+		selectItemInDefaultDropdown(driver, RegisterPageUI.DAY_DROPDOWN, day);
+	}
+
+	public void selectMonthDropDown(String month) {
+		waitForElementVisible(driver, RegisterPageUI.MONTH_DROPDOWN);
+		selectItemInDefaultDropdown(driver, RegisterPageUI.MONTH_DROPDOWN, month);
+	}
+
+	public void selectYearDropDown(String year) {
+		waitForElementVisible(driver, RegisterPageUI.YEAR_DROPDOWN);
+		selectItemInDefaultDropdown(driver, RegisterPageUI.YEAR_DROPDOWN, year);
+	}
+
+	public void enterToEmailTextbox(String emailAddress) {
+		waitForElementVisible(driver, RegisterPageUI.EMAIL_TEXTBOX);
+		sendkeyToElement(driver, RegisterPageUI.EMAIL_TEXTBOX, emailAddress);
+
+	}
+
+	public void enterToCompanyTextbox(String companyName) {
+		waitForElementVisible(driver, RegisterPageUI.COMPANY_TEXTBOX);
+		sendkeyToElement(driver, RegisterPageUI.COMPANY_TEXTBOX, companyName);
+
+	}
+
+	public void enterToPasswordTextbox(String password) {
+		waitForElementVisible(driver, RegisterPageUI.PASSWORD_TEXTBOX);
+		sendkeyToElement(driver, RegisterPageUI.PASSWORD_TEXTBOX, password);
+	}
+
+	public void enterToConformPasswordTextbox(String password) {
+		waitForElementVisible(driver, RegisterPageUI.CONFIRM_PASSWORD_TEXTBOX);
+		sendkeyToElement(driver, RegisterPageUI.CONFIRM_PASSWORD_TEXTBOX, password);
+
+	}
+
+	public void clickToRegisterButton() {
+		waitForElementClickable(driver, RegisterPageUI.REGISTER_BUTTON);
+		clickToElement(driver, RegisterPageUI.REGISTER_BUTTON);
+
+	}
+
+	public void clickToLogoutLink() {
+		waitForElementClickable(driver, RegisterPageUI.LOGOUT_LINK);
+		clickToElement(driver, RegisterPageUI.LOGOUT_LINK);
+	}
+
+	public boolean isRegisterSuccessMessageDisplay() {
+		waitForElementVisible(driver, RegisterPageUI.REGISTERED_SUCCESS_MESSAGE);
+		return isElementDisplayed(driver, RegisterPageUI.REGISTERED_SUCCESS_MESSAGE);
+	}
 
 }
